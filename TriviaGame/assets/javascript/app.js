b4f2db3419b4640c5578949cbbb9ed6cf973b505
Jonathon Wells <jonathonwells10@gmail.com>
2018-06-10 14:50:44 -0500

    js updates

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -60,3 +60,11 @@ var triviaQuestions = [{
 	answer: 0
 }];
 
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "I said a Hip Hop Hooray, that's right!",
+	incorrect: "Inncorrect your flow is weak son!",
+	endTime: "OOPS! Times up!",
+	finished: "Okay, Let's see if you're dope or a dope."
+}
