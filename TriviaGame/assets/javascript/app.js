a4e1c81abdb87b8f5bd9aa4a71ac51beb9a94bfd
Jonathon Wells <jonathonwells10@gmail.com>
2018-06-10 14:29:43 -0500

    copy paste questions

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -2,4 +2,61 @@ var triviaQuestions = [{
 	question: "hip hop trivia?",
 	answerList: ["yes", "no", "maybe", "probably"],
 	answer: 0
-},]
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+},{
+	question: "hip hop trivia?",
+	answerList: ["yes", "no", "maybe", "probably"],
+	answer: 0
+}];
+
