25f025a500181e28fd83f3ba10f9a37e9b8b0d39
Jonathon Wells <jonathonwells10@gmail.com>
2018-06-23 18:26:28 -0500

    inputs adding to page

--- TrainSchedule/assets/javascript.js
+++ TrainSchedule/assets/javascript.js
@@ -17,9 +17,9 @@ var config = {
             ev.preventDefault();
 
             let train = $('#train-input').val();
-            let train = $('#destination-input').val();
-            let train = $('#time-input').val();
-            let train = $('#frequency-input').val();
+            let destination = $('#destination-input').val();
+            let time = $('#time-input').val();
+            let frequency = $('#frequency-input').val();
 
             console.log(train);
             console.log(destination);
@@ -38,29 +38,28 @@ var config = {
 
   database.ref().on('child_added', function (snapshot) {
 
-    let train = snapshot.val()
+    let newTrain = snapshot.val()
 
-    let timeObj = moment(train.time, 'HH:mm');
-    console.log(dateObj);
-    let numMonths = moment().diff(timeObj, 'HH:mm')
+    let timeObj = moment(newTrain.time, 'HH:mm');
+    console.log(timeObj);
+    let minsAway = moment().diff(timeObj, 'HH:mm')
     console.log(minsAway)
-    let rate = parseInt(train.time);
-    let total = timeObj - currentTime;
+ 
 
-    let nameTd = $('<td>' + train.name + '</td>')
-    let destinationTd = $('<td>' + train.destination + '</td>')
-    let timeTd = $('<td>' + train.time + '</td>')
-    let frequencyTd = $('<td>' + train.frequency + '</td>')
+    let trainTd = $('<td>' + newTrain.train + '</td>')
+    let destinationTd = $('<td>' + newTrain.destination + '</td>')
+    let timeTd = $('<td>' + newTrain.time + '</td>')
+    let frequencyTd = $('<td>' + newTrain.frequency + '</td>')
     let minsTd = $('<td>' + minsAway + '</td>')
     
 
     let row = $('<tr>');
-    row.append(nameTd)
+    row.append(trainTd)
     row.append(destinationTd)
     row.append(timeTd)
     row.append(frequencyTd)
     row.append(minsTd)
-    row.append(totalTd)
+    
 
     $('#table-body').append(row);
 
