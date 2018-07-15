4a3c4ba318257f4d06f3ae05f4a472015bbc26c2
Jonathon Wells <jonathonwells10@gmail.com>
2018-07-15 15:25:18 -0500

    added dotenv

--- liri-node-app/keys.js
+++ liri-node-app/keys.js
@@ -1,16 +1,15 @@
 console.log('this is loaded');
 
-exports.twitter = {
+exports.twitterKeys = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
+exports.spotifyKeys = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 var keys = require("./keys.js");
 
 var request = require('request');
