2ea8edbed5f7c1a92d711424ae92ca9ebc550e46
Jonathon Wells <jonathonwells10@gmail.com>
2018-07-30 18:50:24 -0500

    bamazon instructions

--- bamazon/bamazonCustomer.js
+++ bamazon/bamazonCustomer.js
@@ -5,16 +5,30 @@ var inquire = require('inquirer');
 var app = express();
 var PORT = 3000;
 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3000,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon_DB"
+  });
 
-  inquirer
-  .prompt([
-    // Ask if the user wants to Post and Item or Bid on a Current Item
-    {
-        type: "list",
-        message: "Which Item would you like to purchase?",
-        choices: ["item_id"],
-        name:"Bid or Post"
-    }]);
+
+//   inquirer
+//   .prompt([
+//     // Ask if the user wants to Post and Item or Bid on a Current Item
+//     {
+//         type: "list",
+//         message: "Which Item would you like to purchase?",
+//         choices: ["item_id"],
+//         name:"Bid or Post"
+//     }]);
 
 
 
